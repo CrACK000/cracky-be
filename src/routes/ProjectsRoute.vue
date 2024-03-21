f6270614deb7fe615c6f78670b05c5db575bbf96
Patrik Fejfár <patrikfejfar2@gmail.com>
2024-03-21 23:13:35 +0100

    v1.2

--- src/routes/ProjectsRoute.vue
+++ src/routes/ProjectsRoute.vue
@@ -10,6 +10,9 @@ const Projects = ref<Project[]>(projects)
 <template>
 
   <Container>
+    <div class="text-4xl text-blue-500 font-semibold drop-shadow-lg mt-10">
+      Projekty
+    </div>
     <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-12 md:gap-0 lg:gap-6 my-10">
       <router-link v-for="projekt in Projects" class="group xxs:p-4 md:p-6 scale-100 md:hover:scale-105 transition" :to="{ name: 'project', params: { key: projekt.key } }">
         <div class="mb-4 px-3 flex justify-between items-center">
@@ -21,6 +24,22 @@ const Projects = ref<Project[]>(projects)
         <img v-if="projekt.gallery.length" :src="`${projekt.key}/${projekt.gallery[0].image}`" :alt="projekt.name" class="rounded-xl group-hover:rounded-2xl group-hover:shadow-xl transition duration-500">
       </router-link>
     </div>
+
+    <div class="text-4xl text-blue-500 font-semibold drop-shadow-lg mb-6">
+      Balíky
+    </div>
+    <div class="grid lg:grid-cols-2 gap-6 md:gap-8 lg:gap-12">
+      <a href="https://github.com/CrACK000/easy-python-backend-app" target="_blank" class="bg-gray-950 border border-gray-900 rounded-xl p-6 hover:text-blue-500 hover:border-blue-600 transition">
+        <div class="flex items-center gap-x-3 md:text-lg">
+          <i class="fa-brands fa-github fa-xl"></i>
+          Python Authentication API starter
+        </div>
+        <div class="text-sm opacity-75 mt-4">
+          Python Flask API for user management including registration, login, authentication, and profile update.
+        </div>
+      </a>
+    </div>
+
   </Container>
 
 </template>
